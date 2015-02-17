7f69bc79d00b292bf0f2aca9a45e4193ae088abd
joebragg <joe.bragg@hotmail.com>
2015-02-16 23:22:00 -0600

    Added vehicle selection

--- server.R
+++ server.R
@@ -1,7 +1,34 @@
 shinyServer(
-   function(input, output) {
+   function(input, output,session) {
+      
       yrsubset<-reactive({
          subset(vehicle.data,(vehicle.data$Year>=input$yrs[1])&(vehicle.data$Year<=input$yrs[2]))
+      })
+      
+      observe({
+         yr_options<-as.list(input$yrs[1]:input$yrs[2])
+         names(yr_options)<-input$yrs[1]:input$yrs[2]
+         
+         updateSelectInput(session, "yr",
+                           choices = c('None',yr_options),
+                           selected = input$yr)
+         
+         mk_vector<-unique(subset(vehicle.data,(vehicle.data$Year==input$yr))$Make)
+         mk_options<-as.list(as.character(mk_vector))
+         names(mk_options)<-as.character(mk_vector)
+         
+         updateSelectInput(session, "mk",
+                           choices = c('None',mk_options),
+                           selected = input$mk)
+
+         mdl_vector<-unique(subset(vehicle.data,(vehicle.data$Year==input$yr & vehicle.data$Make==input$mk))$Model)
+         mdl_options<-as.list(as.character(mdl_vector))
+         names(mk_options)<-as.character(mdl_vector)
+         
+         updateSelectInput(session, "mdl",
+                           choices = c('None',mdl_options),
+                           selected = input$mdl)
+         
       })
       
       output$plot <- renderPlot({
@@ -9,6 +36,16 @@ shinyServer(
          
          if (input$color != 'None')
             p <- p + aes_string(color=input$color)
+         
+         if (input$smooth)
+            p <- p + geom_smooth()
+         
+         if (input$mdl!="None"){
+            vehsel<-subset(vehicle.data,(vehicle.data$Year==input$yr &
+                                            vehicle.data$Make==input$mk &
+                                            vehicle.data$Model==input$mdl))
+            p <- p + geom_point(data=vehsel,shape=13,size=5,colour="red")
+         }
          
          print(p)
       })
--- ui.R
+++ ui.R
@@ -3,19 +3,29 @@ shinyUI(pageWithSidebar(
    headerPanel("Vehicle Data Explorer"),
    
    sidebarPanel(
+      HTML('See Instructions Below'),
+      
       sliderInput('yrs','Year Range',value = c(1995,2012), min = 1984, max = 2016, step = 1),
       selectInput('x', 'X', names(vehicle.data),selected='Combined_MPG'),
       selectInput('y', 'Y', names(vehicle.data),selected='Fuel_Cost'),
       selectInput('color', 'Color', c('None', names(vehicle.data)),selected='None'),
-      HTML('The data for this Vehicle Data Explorer comes from the CSV file with MPG data
-            from 1984 to 2016 at <a href=http://www.fueleconomy.gov/feg/download.shtml>fueleconomy.gov</a>.
-           <p>Just move the slider for the range of years you wish to see and select the 
-           data to use for the X & Y axis and for the Color of the points.</p>')
+      checkboxInput('smooth', 'Smooth'),
+      selectInput('yr', 'Year', c('None'),selected='None'),
+      selectInput('mk', 'Make', c('None'),selected='None'),
+      selectInput('mdl', 'Model', c('None'),selected='None'),
+      
+      HTML('<p>The data for this Vehicle Data Explorer comes from the CSV file with
+            MPG data from 1984 to 2016 at 
+            <a href=http://www.fueleconomy.gov/feg/download.shtml>fueleconomy.gov</a>.
+           <h3>Instructions</h3>
+           <p>Move the slider for the range of years you wish to see and select the 
+           data to use for the X & Y axis and for the Color of the points. If you would like to see a smoothing 
+           curve, check the Smooth checkbox. Note: Smoothing will only happen if 
+           there are sufficient unique values to support 10 knots.</p>
+           <p>If you would like to see where a particular vehicle is on the plot, select the year,
+           make and model of the vehicle and red cross hatched circle(s) will indicate its position(s).</p>')
    ),
    mainPanel(
-      plotOutput('plot',height="800px"),
-      verbatimTextOutput("xout"),
-      verbatimTextOutput("yout"),
-      verbatimTextOutput("colorout")
+      plotOutput('plot',height="800px")
    )
 ))
