8e2fbe0ea411d1b2f350e7c7cbd517e6a267d900
joebragg <joe.bragg@hotmail.com>
2015-02-17 21:39:42 -0600

    Adding Slidify files

---
+++ Vehicle_Data_Explorer/index.md
@@ -0,0 +1,33 @@
+---
+title       : Vehicle Data Explorer
+subtitle    : 
+author      : Joe Bragg
+job         : 
+framework   : io2012        # {io2012, html5slides, shower, dzslides, ...}
+highlighter : highlight.js  # {highlight.js, prettify, highlight}
+hitheme     : tomorrow      # 
+widgets     : []            # {mathjax, quiz, bootstrap}
+mode        : selfcontained # {standalone, draft}
+knit        : slidify::knit2slides
+---
+
+## Vehicle Data Plots
+
+1. Edit YAML front matter
+2. Write using R Markdown
+3. Use an empty line followed by three dashes to separate slides!
+
+--- .class #id 
+
+## Fuel Consuption & Carbon Foot Print
+
+
+---
+## Sample Plots
+
+---
+## Future Updates
+
+
+
+
--- ui.R
+++ ui.R
@@ -1,20 +1,22 @@
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
    
-   headerPanel("Vehicle Data Explorer"),
+   titlePanel("Vehicle Data Explorer"),
    
-   sidebarPanel(
-      HTML('See Instructions Below'),
+   sidebarLayout(
       
-      sliderInput('yrs','Year Range',value = c(1995,2012), min = 1984, max = 2016, step = 1),
-      selectInput('x', 'X', names(vehicle.data),selected='Combined_MPG'),
-      selectInput('y', 'Y', names(vehicle.data),selected='Fuel_Cost'),
-      selectInput('color', 'Color', c('None', names(vehicle.data)),selected='None'),
-      checkboxInput('smooth', 'Smooth'),
-      selectInput('yr', 'Year', c('None'),selected='None'),
-      selectInput('mk', 'Make', c('None'),selected='None'),
-      selectInput('mdl', 'Model', c('None'),selected='None'),
-      
-      HTML('<p>The data for this Vehicle Data Explorer comes from the CSV file with
+      sidebarPanel(
+         HTML('See Instructions Below'),
+         
+         sliderInput('yrs','Year Range',value = c(1995,2012), min = 1984, max = 2016, step = 1),
+         selectInput('x', 'X', names(vehicle.data),selected='Combined_MPG'),
+         selectInput('y', 'Y', names(vehicle.data),selected='Fuel_Cost'),
+         selectInput('color', 'Color', c('None', names(vehicle.data)),selected='None'),
+         checkboxInput('smooth', 'Smooth'),
+         selectInput('yr', 'Year', c('None'),selected='None'),
+         selectInput('mk', 'Make', c('None'),selected='None'),
+         selectInput('mdl', 'Model', c('None'),selected='None'),
+         
+         HTML('<p>The data for this Vehicle Data Explorer comes from the CSV file with
             MPG data from 1984 to 2016 at 
             <a href=http://www.fueleconomy.gov/feg/download.shtml>fueleconomy.gov</a>.
            <h3>Instructions</h3>
@@ -24,8 +26,10 @@ shinyUI(pageWithSidebar(
            there are sufficient unique values to support 10 knots.</p>
            <p>If you would like to see where a particular vehicle is on the plot, select the year,
            make and model of the vehicle and red cross hatched circle(s) will indicate its position(s).</p>')
-   ),
-   mainPanel(
-      plotOutput('plot',height="800px")
+      ),
+      
+      mainPanel(
+         plotOutput('plot',height="800px")
+      )
    )
 ))
